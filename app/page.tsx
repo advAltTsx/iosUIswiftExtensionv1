27bf1fafde9f5a0d1db2a217702360df59f5f235
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-06 07:52:54 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+import { Button } from "@/components/ui/button"
+
+
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <Drawer shouldScaleBackground>
+      <DrawerTrigger asChild>
+        <Button variant={'link'}><h1 className="scroll-m-30 underline p-6 text-2xl font-extrabold tracking-tight lg:text-5xl">
+      SA-B?
+    </h1></Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="h-[70vh]">
+          <DrawerHeader >
+            <div className='h-auto w-full flex items-center justify-start'>
+            <DrawerClose asChild>
+              <Button variant="ghost"><h1 className=' text-blue-600'>Cancel</h1></Button>
+            </DrawerClose>
+            </div>
+            <div className='flex flex-col items-center justify-center text-center h-full my-10'>
+            <DrawerTitle>Still here</DrawerTitle>
+            <DrawerDescription>How are you?</DrawerDescription>
+            <br />
+            <blockquote className="mt-6">{"Tbh, I fear talking to non-boys."}</blockquote>
+            <blockquote className="mb-6">{"Also, I need your help."}</blockquote>
+            <br />
+            <DrawerTitle>{"I hope you understand"}</DrawerTitle>
+            <DrawerDescription>{"Let's meet IRL"}</DrawerDescription>
+
+            </div>
+          </DrawerHeader>
+          
+          <DrawerFooter>
+            
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+    </main>
+  )
+}
