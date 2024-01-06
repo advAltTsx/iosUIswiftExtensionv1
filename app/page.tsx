7df446cc24dc056ee58bbc1a43fdd7c4cdb180f6
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-06 14:31:47 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       </DrawerContent>
     </Drawer>
     </main>
-    <div className="flex items-center justify-center text-center p-6">views: 489.000 (rounded and provided by Atlassian)</div>
+    <div className="flex items-center justify-center text-center p-6">{"<h2 className='${style}'>$views: 489.000 ($Atlassian_Advertisement_Rounded_Provided)</h2>"}</div>
     </>
       )
 }
