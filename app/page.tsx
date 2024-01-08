c1f9754bf66358b51b1e0b23ec0cd1b40a48496b
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-08 23:21:14 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -55,7 +55,7 @@ export default function Home() {
               <DrawerTitle>I also have to make new friends like you</DrawerTitle>
              <br/>
               <DrawerDescription>The way these years passed, you are not being my friend neither letting me make new friends. Whenever I try talking to someone you just go acting different.</DrawerDescription>
-            <DrawerDescription>{"I'm not angry on you, even a bit. But still, stay safe and be healthy. You made new cool friends. Tell them to put your name into their workarts. I saw who gave actual attention to me when I mentioned the word 'die'. You're not welcomed in any of my works anymore."} </DrawerDescription>
+            <DrawerDescription>{"I'm not angry on you, even a bit. But still, stay safe and be healthy. You made new cool friends. I'm not cool, just an ordinary person, tell them to put your name into their workarts. I saw who gave actual attention to me when I mentioned the word 'die'. You're not welcomed in any of my works anymore."} </DrawerDescription>
             <br />
           </div>
           </DrawerHeader>
