90a088a17d30810b6ada5842950dc70766fde9a1
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-09 16:15:20 +0530

    *

--- app/page.tsx
+++ app/page.tsx
@@ -35,54 +35,17 @@ export default function Home() {
     >
       <CarouselContent className="-mt-1 h-[50vh]">
           
-        <CarouselItem className="pt-1 md:basis-1/2 flex items-center justify-center text-center">
+      
+        
+          <CarouselItem className="pt-1 md:basis-1/2 flex items-center justify-center text-center">
             <Drawer shouldScaleBackground>
       <DrawerTrigger asChild>
-        <Button variant={'link'}><h1 className="scroll-m-30 underline p-6 text-2xl font-extrabold tracking-tight lg:text-5xl">
-      Sugar?
-    </h1></Button>
-      </DrawerTrigger>
-      <DrawerContent>
-        <div className="min-h-[70vh] h-auto px-6">
-          <DrawerHeader >
-            <div className='h-auto w-full flex items-center justify-start'>
-            <DrawerClose asChild>
-              <Button variant="ghost"><h1 className=' text-blue-600'>Close</h1></Button>
-            </DrawerClose>
-            </div>
-            <div className='flex flex-col justify-center text-center h-full my-10'>
-           <DrawerDescription>9/1/24</DrawerDescription>
-              <br/>
-              <DrawerTitle>I realised I fought in the middle of the exams</DrawerTitle>
-             <br/>
-              <DrawerDescription>Forgive me.</DrawerDescription>
-         <br/>
-              <DrawerDescription>All the best.</DrawerDescription>
-     <br/>
-                
-            </div>
-          </DrawerHeader>
-          
-          <DrawerFooter>
-            
-          </DrawerFooter>
-        </div>
-      </DrawerContent>
-    </Drawer>
-
-          </CarouselItem>
-        
-        
-        
-        <CarouselItem className="pt-1 md:basis-1/2 flex items-center justify-center text-center">
-            <Drawer shouldScaleBackground>
-      <DrawerTrigger asChild>
-        <Button variant={'link'}><h1 className="scroll-m-30 underline p-6 text-2xl font-extrabold tracking-tight lg:text-5xl">
+        <Button variant={'link'}> <div className='flex items-center'><h1 className="scroll-m-30 underline p-6 text-2xl font-extrabold tracking-tight lg:text-5xl">
       SA-B?
-    </h1></Button>
+    </h1> <div className=' bg-green-400 border border-green-600 py-2 px-4 rounded-2xl h-auto text-green-950'>Updated</div></div></Button>
       </DrawerTrigger>
       <DrawerContent>
-        <div className="min-h-[70vh] h-auto px-6">
+        <div className="min-h-[70vh] h-auto">
           <DrawerHeader >
             <div className='h-auto w-full flex items-center justify-start'>
             <DrawerClose asChild>
@@ -90,14 +53,26 @@ export default function Home() {
             </DrawerClose>
             </div>
             <div className='flex flex-col justify-center text-center h-full my-10'>
-           <DrawerDescription>8/1/24</DrawerDescription>
+            <DrawerDescription>8/1/24</DrawerDescription>
               <br/>
               <DrawerTitle>You should smile</DrawerTitle>
              <br/>
               <DrawerDescription>Your smile is something precious, the way you smiled that day even I could not resist smiling back</DrawerDescription>
             <DrawerDescription>{"You look so cute, just like"} <Link href={'https://w0.peakpx.com/wallpaper/26/103/HD-wallpaper-monkey-selfie-face-thumbnail.jpg'}> <span className='underline text-blue-600 font-medium'>this</span></Link> 💀</DrawerDescription>
-         <DrawerDescription>All the best.</DrawerDescription>
-              <br />
+            <br />
+            <DrawerDescription>SugarByte/Sugar text was not for you. You are always welcomed. I am angry on them so I am gonna be fighting. After a couple of days even they know I will be welcoming them once again like I always do.</DrawerDescription>
+              <br/>
+            <br />
+            <DrawerDescription>9/1/24</DrawerDescription>
+              <br/>
+              <DrawerTitle>"Humko aise bolta hai? tum toh <Link href={'https://i.pngimg.me/thumb/f/720/m2i8H7H7Z5K9d3A0.jpg'}> 
+              <span className='underline text-blue-600 font-medium'>aisa</span> </Link> dikhta hai"</DrawerTitle>
+              <br/>
+              <DrawerDescription>M sirf mazak karha tha 🥲</DrawerDescription>
+              <DrawerDescription>"Vaise hum..."</DrawerDescription>
+              <DrawerDescription> *sighs*</DrawerDescription>
+              <DrawerDescription>"... mazak nahi kiye"</DrawerDescription>
+              <DrawerDescription>🥲🥲🥲</DrawerDescription>
           </div>
           </DrawerHeader>
           
@@ -109,6 +84,7 @@ export default function Home() {
     </Drawer>
 
           </CarouselItem>
+
 
 
           <CarouselItem className="pt-1 md:basis-1/2 flex items-center justify-center text-center">
