b1b667b04490824545f0b7ff99f804dbcb83d18f
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-12 01:01:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/redirect_blocked',
+        permanent: true,
+      },
+      {
+        source: '/redirect_blocked',
+        destination: 'https://instagram.com/accounts/login',
+        permanent: true,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
