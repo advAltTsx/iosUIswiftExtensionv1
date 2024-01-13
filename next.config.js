38beb42384d345f5f8bb4d59a924ec74dc9519f3
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-13 11:57:19 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,7 @@
 const nextConfig = {
   async redirects() {
     return [
-      {
-        source: '/',
-        destination: '/redirect_blocked',
-        permanent: true,
-      },
-      {
-        source: '/redirect_blocked',
-        destination: 'https://advnote.vercel.app/preview/j574z73xntr9a9026cqm9qw1f56hcghm',
-        permanent: true,
-      },
+      
     ]
   },
 }
