4640dc4594bffb8927c6af657589d3016bfd43f9
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-13 13:56:31 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ADVSnaps",
+  title: "ADVEdges",
   description: "@advwastaken",
   icons: {
     icon: '/logo.jpg'
