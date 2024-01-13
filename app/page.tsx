e080613a54d530edf61e5bd7d0beafe531e96241
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-13 14:33:07 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -52,7 +52,8 @@ export default function Home() {
             <div className='flex flex-col px-4 justify-center text-center h-full my-10'>
               <DrawerDescription>While others are scrolling Instagram Reels, and Youtube Shorts,</DrawerDescription>
               <br/>
-              <DrawerDescription>I decided not to open these apps ever again, and {"that's why,"}</DrawerDescription>
+              <DrawerDescription>I decided not to open these apps ever again, </DrawerDescription>
+              <DrawerDescription>and {"that's why,"}</DrawerDescription>
               <DrawerTitle>{"@adv.vapp is now ADVEdges!"}</DrawerTitle>
               <br/>
             <DrawerDescription>Scroll till the end like the other equivalents.</DrawerDescription>
