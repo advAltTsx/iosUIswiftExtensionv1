adf96231ad614edabf7efd1c551f67e0089216d8
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-15 10:29:02 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -122,10 +122,10 @@ export default function Home() {
                       </div>
                   </Button>
                 </DrawerTrigger>
-                <DrawerContent className="bg-gradient-to-b from-red-700 to-black">
+                <DrawerContent className="bg-gradient-to-b from-[#00FFFF] to-black">
                   <div className="min-h-[70vh] h-auto">
                     <DrawerHeader>
-                      <div className="flex flex-col justify-center bg-black px-6 rounded-2xl border border-red-500 text-center h-full my-10">
+                      <div className="flex flex-col justify-center bg-black px-6 rounded-2xl border border-white/60 text-center h-full my-10">
                         <DrawerTitle>
                           You think that way?
                         </DrawerTitle>
