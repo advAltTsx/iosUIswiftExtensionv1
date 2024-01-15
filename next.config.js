7f2a2bdf4bcbd964eb89cbaa648bc896c8ae5faf
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-16 02:49:22 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     return [
       {
         source: '/',
-        destination: '/redirects/virtuals/alice',
+        destination: '/redirects/virtuals/unauth',
         permanent: true,
       },
     ]
