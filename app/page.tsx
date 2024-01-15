09c12d45f5b77ee890e9bb2bf68f794e8f202c2a
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-16 02:09:33 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -73,43 +73,8 @@ export default function Home() {
             </CarouselItem>
 
             
-              {/*<div  className="h-auto w-full p-5 px-12 flex flex-col space-y-6">
-                          <div className="h-auto w-full  flex justify-between items-center">
-                          <img src="https://i.postimg.cc/QxH531Cx/avg.webp" className="pointer-events-none h-16 w-16 rounded-full border-2 border-purple-500" alt="" />
-                          <h1 className="font-extrabold">Abhyudaya</h1>
-                          <div className=" bg-gray-300 scale-75 border border-white text-gray-950 font-bold py-2 px-4 rounded-2xl h-auto">
-                        Original
-                      </div>
-                          </div>
-                          <div className="h-auto w-full flex items-center justify-center">
-                            <img src="/techs.png" className="pointer-events-none w-4/5 h-auto rounded-2xl border border-purple-800" alt="" />
-                          </div>
-                        </div>
-                      <div className="flex scale-75 flex-col overflow-y-clip overflow-x-clip justify-center text-center h-full my-1">
-                         <DrawerTitle className="font-bold">
-                          {"ADVEdges"}
-                        </DrawerTitle>
-                        <br />
-                        <Command className="bg-black border border-white/60 rounded-2xl">
-                          <CommandInput placeholder="Search Suggestions and Features..." />
-                          <CommandList>
-                            <CommandGroup heading="Suggestions">
-                              <div className="text-left">
-                              <CommandItem>Page stuck? Scroll down like an ordinary Instagram Reel or Youtube Short.</CommandItem>
-                              <CommandItem>Want to close the pop-up? Drag it down or click on the background.</CommandItem>
-                                </div>
-                            </CommandGroup>
 
-                            <CommandSeparator />
-    <CommandGroup heading="Features">
-      <CommandItem>No iPhone? Use an iOS based UI app.</CommandItem>
-      <CommandItem>Low end device? Use a blazing fast UI.</CommandItem>
-    </CommandGroup>
-                            
-                          </CommandList>
-                        </Command> 
-                      </div>
-                      </div>  *}
+                        
 
              
             
