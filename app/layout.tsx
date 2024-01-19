e0ce1d3bfaa91e54993db66c4e940a23e4dbc623
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-19 15:39:49 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ADVEdges: 497K+",
+  title: "ADVEdges: 498K+",
   description: "@advwastaken",
   icons: {
     icon: '/logo.jpg'
