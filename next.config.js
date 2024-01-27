0b50195b4b0023044be2f95a13bf69db1fa16f91
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-01-27 08:06:28 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: 'https://youtube.com/shorts/cplThaynp84?si=MyjI-M61wQgR75UA',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
